8aa6dcd66aadebb38333f3c1a71145d509a59f78
Julio Litzenberg <julio.litzenberg@gmail.com>
2022-03-16 15:13:36 +0100

    Create Rotetad Landing Page UI

--- src/Projects/Rotated/index.js
+++ src/Projects/Rotated/index.js
@@ -1,10 +1,35 @@
 import React from "react";
 import Layout from "../Layout";
+import "./index.css";
 
 function Rotated() {
   return (
     <Layout>
-      <h1>Rotated</h1>
+      <div className="rotated-parent">
+        <div className="row h-100 align-items-center">
+          <div className="col-md-6">
+            <div className="h-75 p-5">
+              <lottie-player
+                src="https://assets3.lottiefiles.com/packages/lf20_enpgdatq.json"
+                background="transparent"
+                speed="1"
+                loop
+                autoplay
+              ></lottie-player>
+            </div>
+          </div>
+          <div className="col-md-6">
+            <div className="rotated-text-part">
+              <h1 data-aos="slide-right">
+                <b>Julio Litzenberg</b>
+              </h1>
+              <p data-aos="slide-left">
+                UI - Web - Mobile <b>Developer</b>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
     </Layout>
   );
 }
