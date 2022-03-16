e803e44f5a4e5b89ed6a9e3632d8033e199aeb8c
Julio Litzenberg <julio.litzenberg@gmail.com>
2022-03-16 14:09:56 +0100

    Create Home Screen UI

--- src/Projects/Home/index.js
+++ src/Projects/Home/index.js
@@ -1,10 +1,42 @@
 import React from "react";
 import Layout from "../Layout";
+import "./index.css";
 
 function Home() {
   return (
     <Layout>
-      <h1>Home</h1>
+      <div className="home-parent">
+        <div className="d-flex parts align-items-center">
+          <div className="part1 text-center text-white d-flex align-items-center justify-content-center flex-column">
+            <h3 className="my-5 ">
+              <b>Frontend Developer</b>
+            </h3>
+            <div className="h-50">
+              <lottie-player
+                src="https://assets1.lottiefiles.com/packages/lf20_m9fz64i8.json"
+                background="transparent"
+                speed="1"
+                loop
+                autoplay
+              ></lottie-player>
+            </div>
+          </div>
+          <div className="part2 text-center text-white d-flex align-items-center justify-content-center flex-column">
+            <div className="h-50">
+              <lottie-player
+                src="https://assets5.lottiefiles.com/packages/lf20_bwbbmn9r.json"
+                background="transparent"
+                speed="1"
+                loop
+                autoplay
+              ></lottie-player>
+            </div>
+            <h3 className="my-5 ">
+              <b>UI - Web - Mobile</b>
+            </h3>
+          </div>
+        </div>
+      </div>
     </Layout>
   );
 }
