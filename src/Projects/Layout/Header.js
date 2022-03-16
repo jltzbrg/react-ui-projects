11507e4537e9cabcbba5eb1c1e547bfbae8c5390
Julio Litzenberg <julio.litzenberg@gmail.com>
2022-03-16 03:22:48 +0100

    Add Responsiveness to the Header/Layout

--- src/Projects/Layout/Header.js
+++ src/Projects/Layout/Header.js
@@ -1,6 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import { HiOutlineMenuAlt1 } from "react-icons/hi";
+import { AiOutlineClose } from "react-icons/ai";
+
 function Header() {
+  const [showMenu, setShowMenu] = useState(false);
+  const path = window.location.pathname;
   const menuItems = [
     {
       title: "Home",
@@ -37,15 +42,30 @@ function Header() {
   ];
   return (
     <>
-      <ul className="menu-items">
-        {menuItems.map((item) => {
-          return (
-            <li>
-              <Link to={`${item.path}`}>{item.title}</Link>
-            </li>
-          );
-        })}
-      </ul>
+      <div className="menu-switch">
+        {showMenu ? (
+          <AiOutlineClose onClick={() => setShowMenu(!showMenu)} />
+        ) : (
+          <HiOutlineMenuAlt1 onClick={() => setShowMenu(!showMenu)} />
+        )}
+      </div>
+
+      {showMenu && (
+        <ul className="menu-items">
+          {menuItems.map((item) => {
+            return (
+              <li>
+                <Link
+                  to={`${item.path}`}
+                  className={`${path === item.path && "active"}`}
+                >
+                  {item.title}
+                </Link>
+              </li>
+            );
+          })}
+        </ul>
+      )}
     </>
   );
 }
